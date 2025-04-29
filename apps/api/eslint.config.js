abb54e14973f6798fe49d74a0aae57e88d6d86e2
sureshunnikrishnan <suresh.techcorp@gmail.com>
2025-04-30 04:56:01 +0530

    Sprint 1

--- apps/api/eslint.config.js
+++ apps/api/eslint.config.js
@@ -2,18 +2,25 @@ import globals from 'globals';
 import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(js.configs.recommended, ...tseslint.configs.recommended, {
-  files: ['**/*.ts'],
-  languageOptions: {
-    ecmaVersion: 2020,
-    sourceType: 'module',
-    globals: {
-      ...globals.node,
+export default tseslint.config(
+  {
+    ignores: ['**/dist/**', '**/node_modules/**'],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      globals: {
+        ...globals.node,
+      },
     },
-  },
-  rules: {
-    'no-console': ['warn', { allow: ['warn', 'error'] }],
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-  },
-});
+    rules: {
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    },
+  }
+);
